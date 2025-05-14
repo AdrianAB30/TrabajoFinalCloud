2e8193c366530dde64c34bf6aef052f0f823b70d
Adrian AB <jose.anchiraico@tecsup.edu.pe>
2025-05-14 12:10:23 -0500

    Input Game 3 Listo

--- src/pages/Game1.tsx
+++ src/pages/Game1.tsx
@@ -1,34 +1,32 @@
-import {Unity, useUnityContext} from "react-unity-webgl";
+import { useEffect } from "react";
+import { Unity, useUnityContext } from "react-unity-webgl";
 
-function Game() {
-    const { unityProvider, sendMessage } = useUnityContext({
-    loaderUrl: "/Juego 1.loader.js",
-    dataUrl: "/Juego 1.data",
-    frameworkUrl: "/Juego 1.framework.js",
-    codeUrl: "/Juego 1.wasm",
+function Game1() {
+    const { unityProvider, unload, sendMessage } = useUnityContext({
+        loaderUrl: "/Juego 1.loader.js",
+        dataUrl: "/Juego 1.data",
+        frameworkUrl: "/Juego 1.framework.js",
+        codeUrl: "/Juego 1.wasm",
     });
 
-    function handleClickSpawnEnemies() {
-        sendMessage("GameObject", "SpawnEnemies");
-    }
+  
+   useEffect(() => {
+    return () => {
+      sendMessage("Audio Manager", "StopMusic"); 
+      unload()
+        .then(() => console.log("✅ Game1 descargado correctamente"))
+        .catch((err) => console.error("❌ Error al descargar Game1:", err));
+    };
+  }, []);
 
     return (
-        <>
-            <div className="centered-container">
-                <div className="centered-content">
-                    <h1 className="centered-title">React + Unity / Tecsup</h1>
-                    <Unity unityProvider={unityProvider} className="centered-unity" />
-
-                    <div className="centered-content">
-                        <button onClick={handleClickSpawnEnemies}>Spawn Enemies</button>
-                    </div>
-
-                </div>
+        <div className="centered-container">
+            <div className="centered-content">
+                <h1 className="centered-title">React + Unity / Tecsup</h1>
+                <Unity unityProvider={unityProvider} className="centered-unity" />
             </div>
-
-        </>
+        </div>
     );
 }
 
-
-export default Game
+export default Game1;
